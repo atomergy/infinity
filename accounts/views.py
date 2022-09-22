9afe43e6c338d318e605948a1cabaa42262d64ba
Navid Rahimzadeh <the.noovidap@proton.me>
2022-09-21 18:18:01 -0700

    set up the accounts app and login view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+
+class NormalizedLoginView(LoginView):
+    """
+    This class is made only to make it easier for developers to manage templates. You can delete the "Normalized" models
+    and save the Templates in the central registration directory (BASE_DIR / "templates").
+    """
+    template_name = "accounts/login.html"
